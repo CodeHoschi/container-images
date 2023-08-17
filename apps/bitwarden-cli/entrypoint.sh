6244011b9e43f3cd999cb8adfc8b61f90548a8ac
CodeHoschi <dawisha@gmail.com>
2023-08-17 03:12:11 +0200

    chore: basic clone from https://github.com/onedr0p/containers

--- apps/bitwarden-cli/entrypoint.sh
+++ apps/bitwarden-cli/entrypoint.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-/app/bw config server ${BW_HOST}
+bw config server ${BW_HOST}
 
-export BW_SESSION=$(/app/bw login ${BW_USER} --passwordenv BW_PASSWORD --raw)
+export BW_SESSION=$(bw login ${BW_USER} --passwordenv BW_PASSWORD --raw)
 
-/app/bw unlock --check
+bw unlock --check
 
 echo 'Running `bw server` on port 8087'
-/app/bw serve --hostname 0.0.0.0 #--disable-origin-protection
+bw serve --hostname 0.0.0.0 #--disable-origin-protection
